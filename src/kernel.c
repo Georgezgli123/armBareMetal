13dac9365c1210aae92e06c41bee156455abef77
George Li <zhiguo.zg.li@gmail.com>
2024-12-24 22:40:20 -0800

    Ported the tiny printf from Kustaa Nyholm for debug purpose.

--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,24 @@
 #include "common.h"
 #include "mini_uart.h"
+#include "printf.h"
+
+void putc(void *p, char c) {
+    if(c == '\n') {
+        uart_send('\r');
+    }
+    uart_send(c);
+}
 
 void kernel_main() {
     uart_init();
-    uart_send_string("Rasperry PI Bare Metal OS init\n");
+    init_printf(0, putc);
+    printf("Rasperry PI Bare Metal OS init\n");
 #if RPI_VERSION == 3
-    uart_send_string("\tBoard: RPI 3\n");
+    printf("\tBoard: RPI 3\n");
 #elif RPI_VERSION == 4
-    uart_send_string("\tBoard: RPI 4");
+    printf("\tBoard: RPI 4");
 #endif
-    uart_send_string("\n\nDone\n");
+    printf("\n\nDone, ported printf is using!\n");
     while(1) {
         uart_send(uart_recv());
     }
